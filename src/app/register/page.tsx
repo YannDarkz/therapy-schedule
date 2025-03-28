9129f41d116049518b3f8a3239617a844b774741
yanndarkz <iansantosmcqueen@gmail.com>
2025-03-28 13:07:10 -0300

    chore:  page register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-// import { supabase } from '../lib/supabase';
 import { UserPlus } from 'lucide-react';
 import type { UserRole } from '@/types';
 

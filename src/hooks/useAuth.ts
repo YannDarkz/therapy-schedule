f0a6061d2b21a34e3f75a5acf53cc374ea613c43
yanndarkz <iansantosmcqueen@gmail.com>
2025-04-28 17:22:02 -0300

    feat: add auth register

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import type { User } from '@/types';
 
 export function useAuth() {
-    const router = useRouter();
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -38,56 +36,36 @@ export function useAuth() {
     }
   };
 
+  const register = async (name: string, email: string, password: string, role: string) => {
+    setLoading(true);
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, password, role }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Erro no registro');
+      }
+
+      const data = await response.json();
+      setUser(data.user);
+      return { error: null };
+
+    } catch (error) {
+      console.error(error);
+      return { error: 'Erro de conexão!' };
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const logout = () => {
     setUser(null);
     return Promise.resolve();
   };
 
-  return { user, loading, login, logout };
+  return { user, loading, login, register, logout };
 }
-
-
-
-// import { useState, useEffect } from 'react';
-// import type { User } from '@/types';
-
-// // Simular usuário local
-// const mockUser: User = {
-//   id: '1',
-//   email: 'usuario@exemplo.com',
-//   name: 'Yannn Teste',
-//   role: 'client'
-// };
-
-// export function useAuth() {
-//   const [user, setUser] = useState<User | null>(null);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     // Simular carregamento
-//     const timer = setTimeout(() => {
-//       setUser(mockUser);
-//       setLoading(false);
-//     }, 1000);
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   //  mandar req para o beck
-//   const login = (email: string, password: string) => { 
-//     setLoading(true);
-//     // Simular login
-//     setTimeout(() => {
-//       setUser(mockUser);
-//       setLoading(false);
-//     }, 1000);
-//     return Promise.resolve({ error: null });
-//   };
-
-//   const logout = () => {
-//     setUser(null);
-//     return Promise.resolve();
-//   };
-
-//   return { user, loading, login, logout };
-// }

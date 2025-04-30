55b72d478dca63499789f11c4f9469266cfe5d74
yanndarkz <iansantosmcqueen@gmail.com>
2025-04-30 20:11:03 -0300

    chore: refactor api login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,10 +5,8 @@ import UserModel from '@/models/User';
 export async function POST(req: Request) {
   try {
     const { email, password } = await req.json();
-    console.log(email);
-    
 
-    await connectDB(); // conecta no MongoDB
+    await connectDB();
 
     const user = await UserModel.findOne({ 'email': email.trim() });
 
@@ -18,12 +16,10 @@ export async function POST(req: Request) {
       return NextResponse.json({ message: 'Usuário não encontrado123' }, { status: 404 });
     }
 
-    // Aqui seria onde você validaria a senha (se ela é igual à do banco)
     if (user.password !== password) {
       return NextResponse.json({ message: 'Senha incorreta' }, { status: 401 });
     }
 
-    // Remover a senha da resposta
     const { password: _, ...userWithoutPassword } = user.toObject();
 
     return NextResponse.json({ user: userWithoutPassword }, { status: 200 });

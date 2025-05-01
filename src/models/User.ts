9d818ba26d0bf4c25589f4cd4c70fc2880064503
yanndarkz <iansantosmcqueen@gmail.com>
2025-05-01 19:20:40 -0300

    chore: refactor user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,29 @@
 import { Schema, model, models } from 'mongoose';
 
+const AppointmentSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  service: {
+    type: String,
+    required: true,
+  },
+  professional: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmado', 'cancelado', 'finalizado'],
+    default: 'pendente',
+  }
+}, { _id: false });
+
 const UserSchema = new Schema({
   name: {
     type: String,
@@ -17,6 +41,7 @@ const UserSchema = new Schema({
   {
     type: String, default: 'client'
   },
+  appointments: [AppointmentSchema]
 
 });
 

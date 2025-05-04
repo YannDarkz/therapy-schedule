2a34e2ce4d808a46c99dafa15b503c2212183e21
yanndarkz <iansantosmcqueen@gmail.com>
2025-05-03 22:47:45 -0300

    chore: refactor type User

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,20 @@
 export type UserRole = 'client' | 'professional';
 
 export interface User {
-  id: string;
+  _id: string;
   email: string;
   role: UserRole;
   name: string;
+  appointments: Appointment[]
 }
 
 export interface Appointment {
   id: string;
-  professional_id: string;
+  professional: string;
+  service: string;
   client_id: string;
   date: string;
+  time: string;
   status: 'pending' | 'confirmed' | 'completed' | 'cancelled';
   created_at: string;
 }
